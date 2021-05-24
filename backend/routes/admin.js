1152bb887c295ab696485c44606b7502584dbd2a
Jeff Li <jeffli@pop-os.localdomain>
2021-05-23 20:29:03 -0400

    added account retrieval

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -8,5 +8,6 @@ router.get('/', adminController.getIndex);
 //Account routes
 router.get('/add-account', adminController.getAddAccount);
 router.post('/add-account', adminController.postAccount);
+router.get('/:accountId', adminController.getAccount);
 
 module.exports = router;
