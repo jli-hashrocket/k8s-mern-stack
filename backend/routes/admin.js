d9e69e74630d2bb40d7d56946c739d2b229557e1
Jeff Li <jeffli@pop-os.localdomain>
2021-05-29 10:32:07 -0400

    Edit and update functionality

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,12 +3,14 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 //Home routes
-router.get('/', adminController.getIndex);
+router.get('/', adminController.index);
 
 //Account routes
-router.get('/add-account', adminController.getAddAccount);
-router.post('/add-account', adminController.postAccount);
-router.get('/:accountId', adminController.getAccount);
-router.post('/delete/:accountId', adminController.postDelete)
+router.get('/add-account', adminController.addAccount);
+router.post('/add-account', adminController.createAccount);
+router.get('/:_id', adminController.getAccount);
+router.get('/edit/:_id', adminController.editAccount);
+router.post('/update/:_id', adminController.updateAccount);
+router.post('/delete/:_id', adminController.deleteAccount)
 
 module.exports = router;
