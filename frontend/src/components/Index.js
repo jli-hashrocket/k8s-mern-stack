b7db66f94c78f05f31fd9075760dfb944bdffcfc
Jeff Li <jeff.li029@gmail.com>
2021-06-11 10:42:17 -0400

    updating styles

---
+++ frontend/src/components/Index.js
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+
+
+const Index = ({}) => {
+  return (
+    <Container className="content-body">
+      <h3>Hello,</h3>
+      Welcome to the Submissions Manager! <br/>You can import submissions data once you've created an account
+    </Container>
+  )
+}
+
+export default Index;
